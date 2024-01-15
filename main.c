71befd49e41cf9b7732c0b90d927e6cf8790474a
kpg <k.petrenkogoljanek.068@studms.ug.edu.pl>
2024-01-15 20:32:27 +0100

    next updates

--- main.c
+++ main.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 #include "raylib.h"
 
-void move(int * x);
+void move(int * x,int * y);
 
 int main ()
 {
     // Position
-    int posX, posY;
+    int posX, posY, windowX, windowY;
+    windowX = 1280;
+    windowY = 960;
     int * pntX;
     int * pntY;
-    posX = 100;
-    posY = 100;
+
+    // Element initial positons
+    posX = windowX/2;
+    posY = windowY/2+300;
     pntX = &posX;
     pntY = &posY;
 
     // Initialization
-    InitWindow(1280,960,"raylib - basic window");
+    InitWindow(windowX,windowY,"raylib - basic window");
 
     SetTargetFPS(60);
     printf("gametemplate");
@@ -26,37 +30,37 @@ int main ()
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
-            DrawRectangle(posX,100,100,100,GOLD);
-            move(pntX);
+            DrawText("Hello Window",(windowX/2-125),(windowY/16+50/2),50,BLACK);
+            DrawRectangle(posX,posY,32,32,GOLD);
+            DrawRectangle(1280/2-125,960/2,250,50,RED);
+            move(pntX,pntY);
+            // collision()
         
         EndDrawing();
     }
     CloseWindow();
     return 0;
 }
-void move(int * x) 
+
+void move(int * x,int * y) 
 {
-    if (IsKeyPressed(KEY_RIGHT))
-    {
-    (*x)+= GetFrameTime() * 1000;
-    }
     if (IsKeyDown(KEY_RIGHT))
     {
-    (*x)+= GetFrameTime() * 1500;
-    }
-
-    if (IsKeyPressed(KEY_LEFT))
-    {
-    (*x)-= GetFrameTime() * 1500;
-    }
-
-    if (IsKeyPressed(KEY_UP))
-    {
     (*x)+= GetFrameTime() * 1000;
     }
 
-    if (IsKeyPressed(KEY_DOWN))
+    if (IsKeyDown(KEY_LEFT))
     {
-    (*x)+= GetFrameTime() * 1000;
+    (*x)-= GetFrameTime() * 1000;
+    }
+
+    if (IsKeyDown(KEY_UP))
+    {
+    (*y)-= GetFrameTime() * 1000;
+    }
+
+    if (IsKeyDown(KEY_DOWN))
+    {
+    (*y)+= GetFrameTime() * 1000;
     }
 }
